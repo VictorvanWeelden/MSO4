c4101ce24a21afb316e1b2ec325d2358a97020e6
6240577 <6240577@UU048144>
2018-10-18 11:47:11 +0200

    v2

---
+++ ShapeDrawing/DrawCanvas.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapeDrawing
+{
+    class DrawCanvas : Drawmethods
+    {
+        public DrawCanvas() : base()
+        {
+
+        }
+
+        public override void DrawLines()
+        {
+            base.DrawLines();
+        }
+    }
+}
---
+++ ShapeDrawing/DrawSVG.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapeDrawing
+{
+    class DrawSVG : Drawmethods
+    {
+        public DrawSVG() : base()
+        {
+
+        }
+
+        public override void DrawLines()
+        {
+            base.DrawLines();
+        }
+    }
+}
